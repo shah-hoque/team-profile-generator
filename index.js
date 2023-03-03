a8a5db1dfe37555df526c6cf4561cdfd94eeac3f
shah_hoque1 <shah.hoque10@gmail.com>
2023-03-03 11:10:05 +0000

    Comments added

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// set the absolute file path of the 'output' dir, using the current module 'index.js' dir path as the starting point
+const outputDir = path.resolve(__dirname, "output");
+// set the absolute file path of a new file 'team.html' inside the 'output' dir
+const outputPath = path.join(outputDir, "team.html");
 
-const render = require("./src/page-template.js");
+const render = require("./page-template.js");
 
+// const askQuestions = () => {
+//   inquirer.prompt([
+//       {
+//           name: "title",
+//           message: "What is the title of your application? \n",
+//           type:"input",
+      
+//       },
+//   ]).then(answers => {
+//       // pass into this promise func the type of file to create & its file content
+  
+//       console.log(answers)
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+//   }).catch((err) => console.error(err));
+// };
 
+// askQuestions()
