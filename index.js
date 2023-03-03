5b4c680813a54d0e2c2b364db6f3bf3a156d531e
shah_hoque1 <shah.hoque10@gmail.com>
2023-03-03 16:42:21 +0000

    Condition added that checks if dir exists

--- index.js
+++ index.js
@@ -6,11 +6,22 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+// FILE CONFIGURATION --------------------------------
+
 // afp = absolute file path
 
-// set the afp of the 'output' dir, using the current module 'index.js' dir path as the starting point
-const outputDir = path.resolve(__dirname, "output");
-// set the afp of a new file 'team.html' inside the 'output' dir
+// set the afp of the 'generated-html' dir, using the current module 'index.js' dir path as the starting point
+const outputDir = path.resolve(__dirname, "generated-html");
+
+// check if the 'generated-html' dir exists, and create it if it does not
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir);
+}
+
+// set the afp of a new file 'team.html' inside the 'generated-html' dir
 const outputPath = path.join(outputDir, "team.html");
 
 const render = require("./page-template.js");
+
+// FILE CONFIGURATION (end) --------------------------------
+
